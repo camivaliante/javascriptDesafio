feda4c64fde92dc8365e6aa5113c347b021dbabf
camiv <camilavaliante_6@hotmail.com>
2022-10-06 19:09:01 -0300

    acomodé los archivos en carpetas

--- artistas.js
+++ artistas.js
@@ -2,25 +2,25 @@ const artistas = [
     {
       nombre: "HARRY STYLES",
       ubicacion: "ESTADIO RIVER PLATE",
-      img1: "./images/harry.jpg",
+      img1: "./assets/images/harry.jpg",
       url: "./events/harry-styles.html",
     },
     {
       nombre: "COLDPLAY",
       ubicacion: "ESTADIO RIVER PLATE",
-      img1: "./images/coldplay.png",
+      img1: "./assets/images/coldplay.png",
       url: "./events/coldplay.html"
     },
     {
       nombre: "IMAGINE DRAGONS",
       ubicacion: "COSTANERA SUR",
-      img1: "./images/imaginedragons.png",
+      img1: "./assets/images/imaginedragons.png",
       url: "./events/imagine-dragons.html"
     },
     {
       nombre: "DUA LIPA",
       ubicacion: "CAMPO ARGENTINO DE POLO",
-      img1: "./images/dualipa.png",
+      img1: "./assets/images/dualipa.png",
       url: "./events/dua-lipa.html"
     }
   ];
